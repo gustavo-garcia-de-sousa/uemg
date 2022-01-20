bcc3ced38018c2ab47ebcf09546801d327ff83f5
Gustavo Garcia <sousagustavogarcia@hotmail.com>
2022-01-19 22:17:06 -0300

    update

--- includes/header.php
+++ includes/header.php
@@ -21,20 +21,26 @@ Inspiração: Alvorada Praia Clube de Frutal.
     <!-- favicon -->
     <link rel="shortcut icon" type="image/png" href="images/favicon.ico">
 
-    <!-- font awesome cdn link  -->
+    <!-- font awesome cdn link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
-    <!-- custom css aos cdn link  -->
+    <!-- custom css aos cdn link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
 
-    <!--Bootstrap cdn link-->
+    <!-- Bootstrap cdn link -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    
-    <!-- custom css file link  -->
+
+    <!-- Full Calendar cdn link -->
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.10.1/locales-all.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.10.1/main.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@5.10.1/main.css">
+
+    <!-- custom css file link -->
     <link rel="stylesheet" href="css/style.css">
 
-    <!-- custom js file link  -->
+    <!-- custom js file link -->
     <script src="js/script.js" defer></script>
+
 
 </head>
 
