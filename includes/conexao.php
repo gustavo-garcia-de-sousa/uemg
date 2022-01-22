36049964cebab6610fd5714eb601fcdde051074a
Gustavo Garcia <sousagustavogarcia@hotmail.com>
2022-01-22 15:33:17 -0300

    update

--- includes/conexao.php
+++ includes/conexao.php
@@ -1,13 +1,22 @@
 <?php
+/*
+https://www.phptutorial.net/php-pdo/pdo-connecting-to-mysql/
+https://dev.mysql.com/doc/workbench/en/
+https://www.php.net/manual
+*/
+
+/*-------- CONFIGURAÇÕES DO SERVIDOR -------- */
 $hostname = '127.0.0.1';
 $username = 'root';
 $password = 'Hz31%9598x0K';
 $database = 'clube_recanto';
+$sql = "mysql:host={$hostname};dbname={$database};";
+$dsn_options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+/*-------- CONFIGURAÇÕES DO SERVIDOR -------- */
 
 try {
-    $connect = new \PDO("mysql:host={$hostname};dbname={$database}", $username, $password);
-    $connect->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-    echo "Conectado com sucesso ao Banco de Dados: {$database}";
-} catch (\PDOException $ex) {
-    echo $ex->getMessage();
+    $pdo = new \PDO($sql, $username, $password, $dsn_options);
+    /*echo "Conexão com {$database} realizada com sucesso!";*/
+} catch (PDOException $error) {
+    echo 'Erro na conexão: ' . $error->getMessage();
 }
