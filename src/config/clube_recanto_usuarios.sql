054013659b06ae236bef32302ddea632e66bcbfd
Gustavo Garcia <sousagustavogarcia@hotmail.com>
2022-02-02 12:19:48 -0300

    update

--- src/config/clube_recanto_usuarios.sql
+++ src/config/clube_recanto_usuarios.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
--- Host: localhost    Database: clube_recanto
+-- Host: localhost    Database: clube_alvorada
 -- ------------------------------------------------------
 -- Server version	8.0.28
 
@@ -23,13 +23,14 @@ DROP TABLE IF EXISTS `usuarios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuarios` (
-  `id_usuarios` int NOT NULL AUTO_INCREMENT,
-  `usuario` varchar(40) DEFAULT NULL,
-  `email` varchar(40) DEFAULT NULL,
-  `senha` varchar(60) DEFAULT NULL,
-  `nivel` int DEFAULT '1',
-  PRIMARY KEY (`id_usuarios`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `id_usuario` int NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(40) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `senha` varchar(32) NOT NULL,
+  `status` int NOT NULL DEFAULT '0',
+  `niveis_acesso` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +39,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'Gustavo Garcia','sousagustavogarcia@hotmail.com','5b67b9cea41a79fcf64bb35f66de6689',1),(2,'123@123.com','123@123.com','d9b1d7db4cd6e70935368a1efb10e377',1);
+INSERT INTO `usuarios` VALUES (1,'gustavo','gustavo@gustavo.com','a6dd42fbbade396ddc9f5c2d61389939',0,1),(2,'jota@jota.com','jota@jota.com','5c67fefc10a53cc13e0a5d0a63d86ef0',0,0),(3,'administrador','administrador@clubealvorada.com','0942beb518062cd47781477e544a786e',0,1);
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -51,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-01-29 11:58:58
+-- Dump completed on 2022-02-02 12:15:53
