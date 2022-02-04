1010aaebe34d8b481df82aa06e0c4bc11b49786f
Gustavo Garcia <sousagustavogarcia@hotmail.com>
2022-02-04 00:16:55 -0300

    update

--- pages/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$database_username = 'root';
-	$database_password = 'Hz31%9598x0K';
-	$conn = new PDO( 'mysql:host=localhost;dbname=clube_recanto', $database_username, $database_password );
-?>
